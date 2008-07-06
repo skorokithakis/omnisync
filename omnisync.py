94a4776457f05dcc416b17fb6790590523b2a7b4
Stavros Korokithakis <stavros@korokithakis.net>
2008-07-06 22:44:13 +0300

    Synchronising single files works.

--- omnisync.py
+++ omnisync.py
@@ -10,6 +10,21 @@ from version import VERSION
 from transports.transportmount import TransportInterface
 
 
+class Configuration:
+    """Hold various configuration options."""
+
+    def __init__(self, options):
+        """Retrieve the configuration from the parser options."""
+        if options.verbosity == 0:
+            logging.getLogger().setLevel(logging.ERROR)
+        elif options.verbosity == 1:
+            logging.getLogger().setLevel(logging.INFO)
+        elif options.verbosity == 2:
+            logging.getLogger().setLevel(logging.DEBUG)
+        self.delete = options.delete
+        print self.delete
+
+
 class OmniSync:
     """The main program class."""
     def __init__(self):
@@ -18,6 +33,8 @@ class OmniSync:
         self.destination = None
         self.source_transport = None
         self.destination_transport = None
+        self.configuration = None
+        self.max_attributes = None
 
         # Initialise the logger.
         logging.basicConfig(level=logging.INFO, format='%(message)s')
@@ -42,17 +59,21 @@ class OmniSync:
             logging.error("The source location \"%s\" does not exist, aborting." %
                           self.source)
             return False
-        if not self.destination_transport.exists(self.destination):
-            logging.debug("The destination location \"%s\" does not exist, creating." %
-                          self.destination)
-            self.destination_transport.mkdir(self.destination)
 
         # Check if both locations are of the same type.
         source_isdir = self.source_transport.isdir(self.source)
-        destination_isdir = self.destination_transport.isdir(self.destination)
+        destination_isdir = self.destination.endswith("/")
         if source_isdir and not destination_isdir:
             logging.error("Source is a directory but destination is a file, aborting.")
             return False
+
+        if not self.destination_transport.exists(self.destination):
+            if self.destination.endswith("/"):
+                logging.debug("The destination location \"%s\" does not exist, creating." %
+                              self.destination)
+                # If the destination should be a directory, create it.
+                self.destination_transport.mkdir(self.destination)
+
         return True
 
     def sync(self, source, destination):
@@ -66,8 +87,89 @@ class OmniSync:
         self.source_transport = self.transports[urlparse.urlsplit(self.source)[0]]()
         self.destination_transport = self.transports[urlparse.urlsplit(self.destination)[0]]()
 
+        # These are the most attributes we can expect from stat calls in these two protocols.
+        self.max_attributes = (self.source_transport.stat_attributes &
+                               self.destination_transport.stat_attributes)
+
         if not self.check_locations():
             return
+
+        # Begin the actual synchronisation.
+        self.recurse()
+
+    def recurse(self):
+        """Recursively synchronise everything."""
+
+        # TODO: Write this to work for actual directories.
+        source_directory_list = self.source_transport.listdir(self.source)
+        source_attributes = {}
+        destination_attributes = {}
+        if source_directory_list:
+            pass
+        else:
+            self.compare_and_copy(self.source,
+                                  self.destination,
+                                  source_attributes,
+                                  destination_attributes
+                                 )
+
+    def compare_and_copy(self, source, destination, source_attributes, destination_attributes):
+        """Compare the attributes of two files and copy if changed.
+
+           source            - A source URL.
+           destination       - A destination URL.
+           source_attributes - A dictionary containing some source attributes.
+
+           Returns True if the file was copied, False otherwise.
+        """
+        if destination.endswith("/"):
+            # source is a file here.
+            destination += source[source.rfind("/")+1:]
+            # If the destination is a file then the source must also be a file,
+            # so just copy one to the other.
+
+        # Try to gather as many attributes of both files as possible.
+        if set(source_attributes) & self.max_attributes < \
+           self.source_transport.stat_attributes & self.max_attributes:
+            # If the set of useful attributes we have is smaller than the set of
+            # useful attributes we can gather through stat(), perform one.
+            logging.debug("Source stat for %s deemed necessary." % source)
+            source_attributes.update(self.source_transport.stat(source))
+        if set(destination_attributes) & self.max_attributes < \
+           self.destination_transport.stat_attributes & self.max_attributes:
+            logging.debug("Destination stat for %s deemed necessary." % destination)
+            destination_attributes.update(self.destination_transport.stat(destination))
+
+        # Compare the keys common in both dictionaries. If any are different, copy
+        # the file.
+        for key in set(source_attributes) & set(destination_attributes):
+            if source_attributes[key] != destination_attributes[key]:
+                logging.debug("Source and destination %s was different (%s vs %s)." %\
+                              (key, source_attributes[key], destination_attributes[key]))
+                logging.info("Copying \"%s\" to \"%s\"..." % (source, destination))
+                self.copy_file(source, destination)
+                self.destination_transport.setattr(destination, source_attributes)
+                return True
+        else:
+            # The two files are identical, skip them.
+            logging.info("Files \"%s\" and \"%s\" are identical, skipping..." %
+                         (source, destination))
+            return False
+
+    def copy_file(self, source, destination):
+        """Copy a file."""
+        # Select the smallest buffer size of the two, to avoid congestion.
+        buffer_size = min(self.source_transport.buffer_size,
+                          self.destination_transport.buffer_size)
+        self.source_transport.open(source, "rb")
+        self.destination_transport.open(destination, "wb")
+        data = self.source_transport.read(buffer_size)
+        while data:
+            self.destination_transport.write(data)
+            data = self.source_transport.read(buffer_size)
+        self.destination_transport.close()
+        self.source_transport.close()
+
 
 omnisync = OmniSync()
 
@@ -82,8 +184,8 @@ def normalise_url(url):
         url = "file://" + url
     return url
 
-def run():
-    """Run the program."""
+def parse_arguments():
+    """Parse the command-line arguments."""
     parser = optparse.OptionParser(
         usage="%prog [options] <source> <destination>",
         version="%%prog %s" % VERSION
@@ -95,22 +197,28 @@ def run():
                       const=0,
                       help="be vewy vewy quiet"
                       )
-    parser.add_option("-v", "--verbose",
+    parser.add_option("-d", "--debug",
                       action="store_const",
                       dest="verbosity",
                       const=2,
-                      help="talk more"
+                      help="talk too much"
+                      )
+    parser.add_option("--delete",
+                      action="store_true",
+                      dest="delete",
+                      default=False,
+                      help="delete extraneous files from destination dirs"
                       )
     (options, args) = parser.parse_args()
-    if options.verbosity == 0:
-        logging.getLogger().setLevel(logging.ERROR)
-    elif options.verbosity == 2:
-        logging.getLogger().setLevel(logging.DEBUG)
     if len(args) != 2:
         parser.print_help()
         sys.exit()
+    return options, args
 
-    # Normalise the URLs before passing them to the module.
+def run():
+    """Run the program."""
+    (options, args) = parse_arguments()
+    omnisync.configuration = Configuration(options)
     omnisync.sync(args[0], args[1])
 
 if __name__ == "__main__":
--- transports/file.py
+++ transports/file.py
@@ -4,15 +4,22 @@ from transports.transportmount import TransportInterface
 
 import os
 import urlparse
+import time
 
 class File(TransportInterface):
     """Plain file access class."""
     # Transports should declare the protocols attribute to specify the protocol(s)
     # they can handle.
     protocols = ("file", )
+    # listdir_attributes is a tuple that contains the file attributes that listdir()
+    # supports.
+    listdir_attributes = set()
+    # Conversely, for stat().
+    stat_attributes = set(("size", "mtime", "atime", "ctime"))
+    # The preferred buffer size for reads/writes.
+    buffer_size = 2**15
 
     def __init__(self):
-        """Initialise the class."""
         self.file_handle = None
 
     def __get_filename(self, url):
@@ -31,7 +38,9 @@ class File(TransportInterface):
 
     def open(self, url, mode):
         """Open a file in _mode_ to prepare for reading."""
-        self.file_handle = open(url, mode)
+        if self.file_handle:
+            raise IOError, "Another file is already open."
+        self.file_handle = open(self.__get_filename(url), mode)
 
     def read(self, size):
         """Read _size_ bytes from the open file."""
@@ -56,22 +65,38 @@ class File(TransportInterface):
     def listdir(self, url):
         """Retrieve a directory listing of the given location.
 
-        Returns a list of (filename, is_directory) tuples if the given URL is a
-        directory, False otherwise
+        Returns a list of (filename, attribute_dict) tuples if the
+        given URL is a directory, False otherwise.
+        attribute_dict is a dictionary of {key: value} pairs for any applicable
+        attributes from ("size", "mtime", "atime", "ctime", "isdir").
         """
         try:
-            items = os.listdir(self.__get_filename(url))
+            return [(x, {}) for x in os.listdir(self.__get_filename(url))]
         except (OSError, WindowsError):
             return False
-        return [(item, os.path.isdir(item)) for item in items]
 
     def isdir(self, url):
-        """Return True if the given URL is a directory, False if it is a file."""
+        """Return True if the given URL is a directory, False if it is a file or
+           does not exist."""
         return os.path.isdir(self.__get_filename(url))
 
     def stat(self, url):
-        """Retrieve various file/directory attributes."""
+        """Retrieve various file/directory attributes.
+
+        Returns a dictionary whose keys are the items of stat_attributes.
+        """
+        try:
+            statinfo = os.stat(self.__get_filename(url))
+        except (OSError, WindowsError):
+            return {"size": None, "mtime": None}
+        return {"size": statinfo.st_size, "mtime": statinfo.st_mtime}
 
     def exists(self, url):
         """Return true if a given path exists."""
         return os.path.exists(self.__get_filename(url))
+
+    def setattr(self, url, attributes):
+        """Set a file's attributes if possible."""
+        # We can only set mtime here.
+        if "mtime" in attributes:
+            os.utime(self.__get_filename(url), (time.time(), attributes["mtime"]))
