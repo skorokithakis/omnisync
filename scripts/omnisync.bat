42bd192b2c342254cf7ac8894bf6a43b6a380519
Stavros Korokithakis <stavros@korokithakis.net>
2008-07-16 18:01:01 +0300

    Created Windows batch file.

--- scripts/omnisync.bat
+++ scripts/omnisync.bat
@@ -1,1 +1,15 @@
+@echo off
+REM = """
+python -x omnisync.bat
+goto end
+"""
+from omnisync.omnisync import OmniSync, parse_arguments
+from omnisync.configuration import Configuration
 
+osync = OmniSync()
+(options, args) = parse_arguments(osync)
+osync.config = Configuration(options)
+osync.sync(args[0], args[1])
+
+"""
+:end """
